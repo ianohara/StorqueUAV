2d26db6370344f9bfcaebce9e01dda93cd4ba9c7
ianohara <ian.ohara@gmail.com>
2011-02-18 21:42:35 -0500

    StateSpaceModel/StorqueStep.m: Began working again on this.  Confusion ensued.  Need to spend a bit more time reading and writing on paper before I can continue, but it is almost there.

--- StateSpaceModel/StorqueStep.m
+++ StateSpaceModel/StorqueStep.m
@@ -1,44 +1,84 @@
 function [sdot] = StorqueStep(s,u)
-% s = state vector (12x1)
+% s = state vector (16x1)
 % sdot = derivative of state at current time
 % u = control input (6x1)
+
+% u = [xd_com yd_com zd_com phi_com theta_com psi_com]
+% Where com = 'commanded'
+xd_com = u(1);
+yd_com = u(2);
+zd_com = u(3);
+
+
+
+% Define physical system parameters
+mass = 5;   % [kg]
+armLen = 0.382;  % [m]
+Ixx = 1;    % [kg*m^2]
+Iyy = 1;    % [kg*m^2]
+Izz = 1;    % [kg*m^2]
+g = 9.81;
+
 % Define all of the system variables
 
-x = s(1);
-y = s(2);
-z = s(3);
+x = s(1);   % World Position x [m]
+y = s(2);   % World position y [m]
+z = s(3);   % World position z [m]
 
-u = s(4);
-v = s(5);
-w = s(6);
+u = s(4);   % Body frame velocity x [m/s]
+v = s(5);   % Body frame velocity y [m/s] 
+w = s(6);   % Body frame velocity z [m/s]
 
-phi = s(7);
-theta = s(8);
-psi = s(9);
+% Euler angles.  The names are slightly wrong
+% because really only psi is on z, while phi and theta are on
+% the transformed versions of themselves. (X' and Y'')
+phi = s(7);    % Euler Angle on body axis x
+theta = s(8);  % Euler angle on body axis y 
+psi = s(9);    % Euler Angle on body axis z
 
-p = s(10);
-q = s(11);
-r = s(12);
 
-Itens = [1 0 0;...  % Inertial tensor
-     0 1 0;...
-     0 0 1];
+p = s(10);  % Angular rate around body axis x
+q = s(11);  % Angular rate around body axis y
+r = s(12);  % Angular rate around body axis z
+
+% Angular rates of props 1-4
+w1 = s(13);   % [rad/s]
+w2 = s(14);   % [rad/s]
+w3 = s(15);   % [rad/s]
+w4 = s(16);   % [rad/s]
+
+% TODO: Fill in correct values here.
+Itens = [Ixx 0 0;...  % Inertial tensor
+         0 Iyy 0;...
+         0 0 Izz];
 
  
-Mtens = m * eye(3,3); % Mass tensor
+Mtens = mass * eye(3,3); % Mass tensor
 
-% Calculate M1-4 and T1-4 from our control model
+% Calculate the desired M1-4 and T1-4
+% For now, just focuse on controlling attitude.  Bruce suggests
+% angular acceleration control, and Dan Mellinger does exactly the same
+% so this is a good starting point.
 
-% Apply motor response delay
+% Moment = (Angular Accel Desired) * Moment of Inertia
+MomCont(1) = (kpRoll*(phi_com - phi) + kdRoll*p) * Ixx;     % 
+MomCont(2) = (kpRoll*(theta_com - theta) + kdRoll*q) * Iyy; % 
+MomCont(3) = 0;  % What's a good idea for yaw?
+
+% Spatial Control
+
+% Calculate the desired angular velocities to achieve these results.
+
+% Form the 
 
 % 3x3 Rotation matrix from body to world fram in the Z-X-Y system (Psi, Phi, Theta)
 R = [ cos(psi)*cos(theta) - sin(phi)*sin(psi)*sin(theta), cos(theta)*sin(psi) + cos(psi)*sin(phi)*sin(theta), -cos(phi)*sin(theta);...
 -cos(phi)*sin(psi),  cos(phi)*cos(psi),  sin(phi);...
 cos(psi)*sin(theta) + cos(theta)*sin(phi)*sin(psi), sin(psi)*sin(theta) - cos(psi)*cos(theta)*sin(phi),  cos(phi)*cos(theta)];
 
-linAccel = inv(Mtens)*([0 0 m*g]' + R*[0 0 -(T1+T2+T3+T4)]');
+linAccel = inv(Mtens)*([0 0 mass*g]' + R*[0 0 -(T1(w1)+T2(w2)+T3(w3)+T4(w4))]');
 
-omegaDot = inv(Itens)*([armLen(T4-T3) armLen(T1-T2) M1+M2+M3+M4] - cross(s(10:12)', Itens*s(10:12)'));
+omegaDot = inv(Itens)*([armLen*(T4(w1)-T3(w1)) armLen*(T1(w1)-T2(w1)) sum(Mom)]' - cross(s(10:12)', Itens*s(10:12)'));
 
 % Matrix that brings you from your angular accels to derivatives of euler
 % angles.
