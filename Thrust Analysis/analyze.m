a69231ec22d430c8f6663ca528d6c3d5818e3f1b
ianohara <ian.ohara@gmail.com>
2011-02-18 20:22:46 -0500

    Created new branch to refactor the thrust data analysis script.
    Thrust Analysis/analyze.m: Made arguments more logical (Specify motor folder, prop name, and trial number).  Also changed the use of huge repetitive concatenations for each csvread() by just making one variable hold the largest string common to all of these calls.  Added some commenting, and fixed the header to accuractely reflect the changes and the previously mentioned addition of option parsing.
    
    Thrust Analysis/workingAnalyzeCalls.m: Works with the new version of analyze.m.

--- Thrust Analysis/analyze.m
+++ Thrust Analysis/analyze.m
@@ -1,42 +1,48 @@
 %------------------------------------------------------------------------
 %         Propeller Thrust Analysis Script (Static Only)
 %           Purpose: Take data in 9 .csv files generated
-%                    by run our thrust test setup for
+%                    by running our thrust test setup for
 %                    a certain propeller on a certain
 %                    motor and generate pretty plots
 %                    of:
 %                       1) Fz vs RPM
 %                       2) Mz vs RPM
-%                       3) |Fz + Fy + Fx| vs Time (line for each duty fac)
-%                       4) |Mz + My + Mx| vs Time (line for each duty fac)
-%                       5) I vs RPM
-%                       6) Power vs RPM
-%                       7) Motor Efficiency vs RPM
-%                       8) Prop Efficiency vs RPM (ideal power for thrust /
+%                       3) |Fz + Fy + Fx| vs Time (point for each duty fac)
+%                       4) |Mz + My + Mx| vs Time (point for each duty fac)
+%                       5) Motor Efficiency vs RPM
+%                       6) Prop Figure of Merit vs RPM (ideal power for thrust /
 %                               actual power for thrust)
-%                       9)
-%                      10) 
+%                       7) |F| vs duty cycle sent to ESC
+%                       8) |M| vs duty cycle sent to ESC
 %
 %   Calling procedure:
-%     analyze('propFolder', 'propName', trialNum) % Pretty self explanatory
+%     analyze('motorFolder', 'propName', trialNum) % Pretty self explanatory
 %           Note: Don't include trailing backslashes.
-%           - propFolder will be used both as the folder name
-%               and as the csv file names, so we need to make sure this
-%               is consistent with Sebastian's script.
-%           - propName is used in the main titles of the plots.  It's
-%               just there so that we can use less cryptic names for our
-%               figures ('Master Airscrew 9x7 Tri' instead of MA0970TP)
+%           - motorFolder is the folder in which the there are sub folders
+%               that are Prop Names.
+%           - propName is the name of the prop used both as the prop folder
+%               name and the prefix of the .csv files contained in said
+%               folder.
 %   
+%       Calling options (specified after the 3 necessary args as '-<char>':
+%               -c : Don't clear/close all figures when called.  Useful
+%                    for laying different trials on top of each other
+%               -v : Verbose (Does nothing! TODO)
+%               -d : Debug mode (Does nothing! TODO)
+%       
 %  Notes: Right now, you need to manually enter the diameter of each prop
 %  you run.  This is horrible.  If we decide on a common naming technique
 %  such as 2 letters for the company, 2 digits for prop length, 1 digit for
 %  prop pitch, and whatever after then we can just pull this out of
-%  propFolder. (This naming convention would be the same as the example
+%  motorFolder. (This naming convention would be the same as the example
 %  cited above, MA0970TP.)
 %------------------------------------------------------------------------
 
-function success = analyze(propFolder, propName, trialNum, varargin)
+function success = analyze(motorFolder, propName, trialNum, varargin)
     
+    % Initialize success to a failure state (>0 = success)
+    success = 0;
+
     % Parse optional arguments and make sure there are at least 3
     % arguments.
     
@@ -82,62 +88,79 @@ function success = analyze(propFolder, propName, trialNum, varargin)
     ampSlope = avgSlope;  %[A/V] -> Convert amp readings from volts->amps
     ampIntercept = avgIntercept;
     
+    % Construct strings from the arguments that will be used as directory
+    % locations and as a quick method of constructing the names of the
+    % various data files we need to access.
+    
+    % Folder containing data folders for each trial run with this
+    % motor/prop combo
+    trialFolder = [motorFolder '/' propName];
+    
     trialString = ['Trial_' num2str(trialNum)];
-    trialFolder = [propFolder '\' trialString];
+    
+    dataFolder = [trialFolder '/' trialString];
+    
+    fileNameStub = [dataFolder '/' propName '_' sprintf('%d',trialNum)];
     
     rho = 1.2;           % [kg/m^3];
     diam = 0.2286;       % [m] THIS NEEDS TO BE PULLED OUT OF propName eventually
     voltage = 11.1;      % [V]
     % END DEFINES %
     
-    if (exist(propFolder,'file') ~= 7)
-        disp(sprintf('Error: Folder "%s" does not exist.',propFolder));
+    if (exist(trialFolder,'file') ~= 7)
+        disp(sprintf('Error: Folder "%s" does not exist.',trialFolder));
         success = -1;
         return;
     end
     
-    if (exist(trialFolder,'file') ~= 7)
-        disp(sprintf('Error: Folder "%s" does not exist.',trialFolder));
+    if (exist(dataFolder,'file') ~= 7)
+        disp(sprintf('Error: Folder "%s" does not exist.',dataFolder));
         success = -2;
         return;
     end
     
     % Check to see if there are data files for transient tests.
     staticTests = 0;
-    if (exist([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Fx'],'file') == 2)
+    if (exist([fileNameStub '_Fx'],'file') == 2)
         staticTests = 1;
     end
     
     % Check to see if there are data files for transient tests.
     transientTests = 0;
-    if (exist([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Fx_trans'],'file') == 2)
+    if (exist([fileNameStub '_Fx_trans'],'file') == 2)
         transientTests = 1;
     end
+    
+    % Construct a string that represents the directory path to the folder
+    % containing all of the CSV files.
     
     if (staticTests == 1)
         disp('Loading static test data...');
         % Load all of our data from .csv files
-        dataFx = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Fx']);     % [N]
-        dataFy = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Fy']);     % [N]
-        dataFz = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Fz']);     % [N]
-        dataTx = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Tx']);     % [mN*m]
-        dataTy = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Ty']);     % [mN*m]
-        dataTz = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Tz']);     % [mN*m]
-        % Number given to the timer in the Maevarm 
-        dataOCR1A = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_dc_nums']);    
+        dataFx = csvread([fileNameStub '_Fx']);     % [N]
+        dataFy = csvread([fileNameStub '_Fy']);     % [N]
+        dataFz = csvread([fileNameStub '_Fz']);     % [N]
+        dataTx = csvread([fileNameStub '_Tx']);     % [mN*m]
+        dataTy = csvread([fileNameStub '_Ty']);     % [mN*m]
+        dataTz = csvread([fileNameStub '_Tz']);     % [mN*m]
+        
+        % Number given to the timer in the Maevarm as the switch low point
+        % in the duty cycle.  
+        dataOCR1A = csvread([fileNameStub '_dc_nums']);    
                                 
         % Note: Current data is given to us in voltage across
         %       a hall effect sensor.  So, we need a calibration factor
         %       to convert from voltage -> amps.  Luckily this
         %       is a linear relationship.
-        dataCur = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_cur']);   % [V]
-        % Convert current data to amps
+        dataCur = csvread([fileNameStub '_cur']);   % [V]
+        
+        % Convert current data to amps from our calibration data
         dataCur = ampSlope*dataCur + ampIntercept;                             % [A]
 
-        dataRPM = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_w']);     % [RPM]
-        dataRPMTime = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_t_w']); % [s]
+        dataRPM = csvread([fileNameStub '_w']);       % [RPM]
+        dataRPMTime = csvread([fileNameStub '_t_w']); % [s]
 
-        setup = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_setup']);
+        setup = csvread([fileNameStub '_setup']);
 
         numTests = setup(1,1);
         durTests = setup(1,2);
@@ -169,10 +192,14 @@ function success = analyze(propFolder, propName, trialNum, varargin)
         avgRPM = sum(dataRPM)./size(dataRPM,1);                     % [RPM]
         avgCur = sum(dataCur)./size(dataCur,1);                     % [A]
         
-        % Perform a linerar fit on the magnitude of thrust for
+        % Perform a linear fit on the magnitude of thrust for
         % purposes of our first linear simulink model.  (It looks
         % more like a 2nd order polynomial)
         avgFMag = sqrt(avgFx.^2 + avgFy.^2 + avgFz.^2);
+        
+        % Now perform a 2nd order fit on the magnitude of thrust versus
+        % dataPwmCom
+        coeffsFitF2nd = polyfit(dataPwmCom, avgFMag,2);
         
         % Perform a linear fit on the  moment magnitude data
         magnitudeM = sqrt(avgTx.^2 + avgTy.^2 + avgTz.^2);
@@ -216,7 +243,6 @@ function success = analyze(propFolder, propName, trialNum, varargin)
       hold on;
       title('Magnitude of F Vector','FontSize',14);
       xlabel('\omega [RPM]','FontSize',14);
-      %ylabel('|\bar{F}| [N]','interpreter','tex');
       ylabel('|F| [N]','interpreter','tex','FontSize',14);
 
       % Magnitude of M
@@ -228,7 +254,6 @@ function success = analyze(propFolder, propName, trialNum, varargin)
 
       title('Magnitude of M Vector','FontSize',14);
       xlabel('\omega [RPM]','FontSize',14);
-      %ylabel('$|\bar{M}| [N]$','interpreter','tex');
       ylabel('|M| [mN*m]','interpreter','tex','FontSize',14);
     
       legend('Average Moment', sprintf('Linear fit (M = %2.4f * w + %2.1f)', coeffsFitM(1), coeffsFitM(2)),'Location','NorthWest');
@@ -309,25 +334,26 @@ function success = analyze(propFolder, propName, trialNum, varargin)
     if (transientTests == 1)
         disp('Loading transient test data...');
         % Load all of our data from .csv files
-        dataTranFx = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Fx_trans']);     % [N]
-        dataTranFy = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Fy_trans']);     % [N]
-        dataTranFz = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Fz_trans']);     % [N]
-        dataTranTx = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Tx_trans']);     % [mN*m]
-        dataTranTy = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Ty_trans']);     % [mN*m]
-        dataTranTz = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_Tz_trans']);     % [mN*m]
-        dataTranDuty = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_dc_nums_trans']);
+        dataTranFx = csvread([fileNameStub '_Fx_trans']);     % [N]
+        dataTranFy = csvread([fileNameStub '_Fy_trans']);     % [N]
+        dataTranFz = csvread([fileNameStub '_Fz_trans']);     % [N]
+        dataTranTx = csvread([fileNameStub '_Tx_trans']);     % [mN*m]
+        dataTranTy = csvread([fileNameStub '_Ty_trans']);     % [mN*m]
+        dataTranTz = csvread([fileNameStub '_Tz_trans']);     % [mN*m]
+        dataTranDuty = csvread([fileNameStub '_dc_nums_trans']);
+        
         % Note: Current data is given to us in voltage across
         %       a hall effect sensor.  So, we need a calibration factor
         %       to convert from voltage -> amps.  Luckily this
         %       is a linear relationship.
-        dataTranCur = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_cur_trans']);   % [V]
+        dataTranCur = csvread([fileNameStub '_cur_trans']);   % [V]
         % Convert current data to amps
         dataTranCur = ampSlope*dataTranCur + ampIntercept;                             % [A]
 
-        dataTranRPM = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_w_trans']);     % [RPM]
-        dataTranRPMTime = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_t_w_trans']); % [s]
+        dataTranRPM = csvread([fileNameStub '_w_trans']);     % [RPM]
+        dataTranRPMTime = csvread([fileNameStub '_t_w_trans']); % [s]
 
-        setupTran = csvread([trialFolder '\' propName '_' sprintf('%d',trialNum) '_setup_trans']);
+        setupTran = csvread([fileNameStub '_setup_trans']);
 
         numTestsTran = setupTran(1,2);
         durTestsTran = setupTran(1,3);
@@ -368,9 +394,6 @@ function success = analyze(propFolder, propName, trialNum, varargin)
       tTran = 1/sampleRateTran:1/sampleRateTran:durTestsTran;
       tTran = tTran';
 
-      size(tTran)
-      size(dataTranFx)
-
       figure(4);
       title(sprintf('Transient Test for Prop: %s',propName), 'FontSize',14);
       plot(tTran,sqrt(dataTranFx.^2+dataTranFy.^2+dataTranFz.^2),'.');
@@ -379,4 +402,6 @@ function success = analyze(propFolder, propName, trialNum, varargin)
       xlabel('Time [s]','FontSize',14);
       ylabel('|F| [N]', 'FontSize',14);
     end
+    
+    success = 1;  % Success!
 end
