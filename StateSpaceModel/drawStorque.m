dbf9ea0ebb67121f245fd04b2bfff6396e9235ff
ianohara <ian.ohara@gmail.com>
2011-02-25 19:50:06 -0500

    Began re-writing this to use patches instead of lines as the graphic for the Storque.  Not complete yet, so drawStorque is broken on this branch for now.

--- StateSpaceModel/drawStorque.m
+++ StateSpaceModel/drawStorque.m
@@ -12,43 +12,24 @@ function success = drawStorque(fh, pos, angles)
     
     cla(fh);
     
-    % Initialize the graphics objects we'll use to represent the storque
-    % Later on we can get fancy with these.
-    fuselageLine = line([0 1], [0 0], [0 0]);
-    rotor1Line = line([0 0], [0 0], [0 0]);
-    rotor2Line = line([0 0], [0 0], [0 0]);
-    rotor3Line = line([0 0], [0 0], [0 0]);
-    rotor4Line = line([0 0], [0 0], [0 0]);
-    
-    % Set the graphics objects properties for the storque
-    set(fuselageLine,'LineWidth',8);
-    
-    set(rotor1Line,'LineWidth',4);
-    set(rotor1Line,'MarkerSize',10);
-    
-    set(rotor2Line,'LineWidth',4);
-    set(rotor2Line,'MarkerSize',10);
-    
-    set(rotor3Line,'LineWidth',4);
-    set(rotor3Line,'MarkerSize',10);
-    
-    set(rotor4Line,'LineWidth',4);
-    set(rotor4Line,'MarkerSize',10);
-
-    % Set some storqueUAV size parameters
-    rotorArmLength = 0.53;
-    fuselageLength = 0.914;
-    
-    % These vectors define the fuselage and 4 rotor arms sitting at the
-    % origin in the world frame (pre-translation and pre-rotation)
-    
     fuselage = [0 0 -fuselageLength];
     rotorArm1 = [rotorArmLength 0 0];
     rotorArm2 = [-rotorArmLength 0 0];
     rotorArm3 = [0 rotorArmLength 0];
     rotorArm4 = [0 -rotorArmLength 0];
     
-    fuselageTrans = eulerRotate(fuselage',angles(1),angles(2),angles(3));
+    xPatchDat = [-0.05; ...
+                 0.05; ...
+                 0.05; ...
+                 -0.05];
+    yPatchDat = [0.382; ...
+                 0.382; ...
+                 -0.382; ...
+                 -0.382];
+    zPatchDat = [0;0;0;0];
+    
+    patch = patch(xPatchDat, yPatchDat, zPatchDat,'r');
+    
     rotorArms1Trans = eulerRotate(rotorArm1',angles(1),angles(2),angles(3));
     rotorArms2Trans = eulerRotate(rotorArm2',angles(1),angles(2),angles(3));
     rotorArms3Trans = eulerRotate(rotorArm3',angles(1),angles(2),angles(3));
@@ -56,10 +37,6 @@ function success = drawStorque(fh, pos, angles)
     
     % Now change the line data for our 4 lines to match the updated
     % position and orientation
-    
-    set(fuselageLine, 'XDATA',[pos(1) pos(1) + fuselageTrans(1)], ... 
-        'YDATA', [pos(2) pos(2) + fuselageTrans(2)], ...
-        'ZDATA', [pos(3) pos(3) + fuselageTrans(3)]);
     
     set(rotor1Line, 'XDATA',[pos(1) pos(1) + rotorArms1Trans(1)], ...
         'YDATA', [pos(2) pos(2) + rotorArms1Trans(2)], ... 
